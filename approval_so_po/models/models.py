db036fd31d70405a6e708cce0c1b8582d49b823f
fazeel-shahzad <81213993+fazeel-shahzad@users.noreply.github.com>
2021-04-05 13:03:16 +0500

    Update models.py

--- approval_so_po/models/models.py
+++ approval_so_po/models/models.py
@@ -36,13 +36,14 @@ class StockScrapInh(models.Model):
                 application.x_css = False
 
 
-# class StockMoveLineInh(models.Model):
-#     _inherit = 'stock.move.line'
+class StockMoveLineInh(models.Model):
+    _inherit = 'stock.move.line'
 
-#     @api.onchange('product_id')
-#     def onchange_product_id(self):
-#         if self.picking_id.origin:
-#             raise UserError('You cannot add Product in this Stage')
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        if self.picking_id.sale_id:
+            if self.picking_id.origin:
+                raise UserError('You cannot add Product in this Stage')
 
 
 class StockMoveInh(models.Model):
