9820285b0acf2e8ae7563c6f4006049ea1769848
fazeel-shahzad <81213993+fazeel-shahzad@users.noreply.github.com>
2021-05-26 11:10:19 +0500

    Update __manifest__.py

--- disc_on_so_header__multi_select/__manifest__.py
+++ disc_on_so_header__multi_select/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'sale_management'],
+    'depends': ['base','product', 'account', 'sale_management'],
 
     # always loaded
     'data': [
