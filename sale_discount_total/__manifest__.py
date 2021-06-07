0bddff9ad96e76c6eddffc2113f66db719d7c019
fazeel-shahzad <81213993+fazeel-shahzad@users.noreply.github.com>
2021-06-07 22:22:51 +0500

    Add files via upload

--- sale_discount_total/__manifest__.py
+++ sale_discount_total/__manifest__.py
@@ -1,34 +1,13 @@
-# -*- coding: utf-8 -*-
-#############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2020-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#    Author: Faslu Rahman(odoo@cybrosys.com)
-#
-#    You can modify it under the terms of the GNU AFFERO
-#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
-#
-#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
-#    (AGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
+
 
 {
     'name': 'Sale Discount on Total Amount',
     'version': '14.0.1.1.0',
     'category': 'Sales Management',
-    'live_test_url': 'https://www.youtube.com/watch?v=CigmHe9iC4s&feature=youtu.be',
     'summary': "Discount on Total in Sale and Invoice With Discount Limit and Approval",
-    'author': 'Cybrosys Techno Solutions',
-    'company': 'Cybrosys Techno Solutions',
-    'website': 'http://www.cybrosys.com',
+    'author': 'Fazeel',
+    'company': 'abc',
+    'website': 'http://www.abc.com',
     'description': """
 
 Sale Discount for Total Amount
@@ -47,7 +26,7 @@ Module to manage discount on total amount in Sale.
         'views/res_config_view.xml',
 
     ],
-    'images': ['static/description/banner.png'],
+    # 'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
     'application': True,
     'installable': True,
