7a089fc86fe19d87486bb3be8d7c3b08a7ce2c29
fazeel-shahzad <81213993+fazeel-shahzad@users.noreply.github.com>
2021-06-14 12:54:36 +0500

    Add files via upload

--- so_po_customization/models/sale.py
+++ so_po_customization/models/sale.py
@@ -57,6 +57,7 @@ class SaleOrderInh(models.Model):
             qty = str(round(qty, 2)) + ' ' + product_qty.uom_id.name
         return qty
 
+    @api.depends('order_line')
     def compute_taxes(self):
         flag = False
         for rec in self.order_line:
--- so_po_customization/models/stock.py
+++ so_po_customization/models/stock.py
@@ -91,22 +91,22 @@ class StockPickingInh(models.Model):
 class StockMoveLineInh(models.Model):
     _inherit = 'stock.move.line'
 
-    remarks = fields.Char("Remarks", compute='_compute_remarks')
-    number = fields.Integer(compute='_compute_get_number', store=True)
-
-    def _compute_remarks(self):
-        for rec in self:
-            if rec.picking_id.sale_id:
-                for line in rec.picking_id.sale_id.order_line:
-                    if rec.product_id.id == line.product_id.id:
-                        rec.remarks = line.remarks
-
-            elif rec.picking_id.purchase_id:
-                for line in rec.picking_id.purchase_id.order_line:
-                    if rec.product_id.id == line.product_id.id:
-                        rec.remarks = line.remarks
-            else:
-                rec.remarks = ''
+    # remarks = fields.Char("Remarks", compute='_compute_remarks')
+    # number = fields.Integer(compute='_compute_get_number', store=True)
+    #
+    # def _compute_remarks(self):
+    #     for rec in self:
+    #         if rec.picking_id.sale_id:
+    #             for line in rec.picking_id.sale_id.order_line:
+    #                 if rec.product_id.id == line.product_id.id:
+    #                     rec.remarks = line.remarks
+    #
+    #         elif rec.picking_id.purchase_id:
+    #             for line in rec.picking_id.purchase_id.order_line:
+    #                 if rec.product_id.id == line.product_id.id:
+    #                     rec.remarks = line.remarks
+    #         else:
+    #             rec.remarks = ''
 
     @api.depends('picking_id')
     def _compute_get_number(self):
