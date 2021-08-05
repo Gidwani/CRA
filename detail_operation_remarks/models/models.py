43c4218eaec42e6c6efc507134857cfd8d1f3463
fazeelshahzad <49979617+fazeelshahzad@users.noreply.github.com>
2021-08-05 11:27:56 +0500

    Add files via upload

--- detail_operation_remarks/models/models.py
+++ detail_operation_remarks/models/models.py
@@ -196,3 +196,15 @@ class StockPickingInh(models.Model):
         for line in self.move_ids_without_package:
             line.quantity_done = 0
         self.is_done_added = False
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        result = super(StockPickingInh, self).fields_view_get(
+            view_id=view_id, view_type=view_type, toolbar=toolbar,
+            submenu=submenu)
+        if view_type == 'form':
+            for repo in result['toolbar']['print']:
+                if repo['name'] == 'Delivery Slip':
+                    result['toolbar']['print'].remove(repo)
+            result['toolbar']['print'].reverse()
+        return result
