a2cd593ab513242c0d6a9e8eb790db68f9939711
fazeelshahzad <49979617+fazeelshahzad@users.noreply.github.com>
2021-08-13 22:44:12 +0500

    Add files via upload

--- detail_operation_remarks/detail_operation_remarks/__manifest__.py
+++ detail_operation_remarks/detail_operation_remarks/__manifest__.py
@@ -19,13 +19,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock'],
+    'depends': ['base', 'stock', 'sale', 'purchase', 'account'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        # 'views/templates.xml',
     ],
 
 }
