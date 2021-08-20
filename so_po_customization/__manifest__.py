f245c98357cf2304f497885cbe7bc10c94771197
fazeelshahzad <49979617+fazeelshahzad@users.noreply.github.com>
2021-08-20 17:57:02 +0500

    Add files via upload

--- so_po_customization/__manifest__.py
+++ so_po_customization/__manifest__.py
@@ -35,7 +35,7 @@
         'reports/delivery_report.xml',
         'reports/stock_picklist.xml',
         'wizard/sale_wizard.xml',
-        'wizard/select_products_wizard_view.xml',
+        # 'wizard/select_products_wizard_view.xml',
     ],
 
 }
--- so_po_customization/models/purchase.py
+++ so_po_customization/models/purchase.py
@@ -19,16 +19,6 @@ class PurchaseOrderInh(models.Model):
             'view_mode': 'form',
         }
 
-    def action_select_products(self):
-        return {
-            'type': 'ir.actions.act_window',
-            'name': 'Select Products',
-            'view_id': self.env.ref('so_po_customization.view_select_products_form', False).id,
-            # 'context': {'default_ref': self.name, 'default_order_amount': self.amount_total, 'default_user_id': self.user_id.id},
-            'target': 'new',
-            'res_model': 'select.products',
-            'view_mode': 'form',
-        }
 
 
 class PurchaseOrderLineInh(models.Model):
