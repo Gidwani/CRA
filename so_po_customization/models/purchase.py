834adc5abc6577f1f1a68df79b1eea5000785a1a
fazeelshahzad <49979617+fazeelshahzad@users.noreply.github.com>
2023-06-01 20:15:41 +0500

    Add files via upload

--- so_po_customization/models/purchase.py
+++ so_po_customization/models/purchase.py
@@ -17,12 +17,6 @@ class PurchaseOrderInh(models.Model):
         rec.action_po_update_subtotal()
         return rec
 
-    # def write(self, vals_list):
-    #     rec = super().write(vals_list)
-    #     if
-    #     self.action_po_update_subtotal()
-    #     return rec
-
     def action_po_update_subtotal(self):
         for rec in self:
             subtotal = 0
@@ -45,7 +39,6 @@ class PurchaseOrderInh(models.Model):
                         amount += rec.vat_amount
 
             order.net_tax = amount
-
 
     @api.depends('discount_rate', 'discount_type', 'subtotal_amount')
     def compute_percentage(self):
@@ -132,7 +125,7 @@ class PurchaseOrderLineInh(models.Model):
             amount = 0
             for tax in rec.taxes_id:
                 amount = amount + tax.amount
-            rec.vat_amount = (amount / 100) * rec.price_unit
+            rec.vat_amount = (amount * rec.product_qty / 100) * rec.price_unit
 
     @api.depends('sequence', 'order_id')
     def _compute_get_number(self):
