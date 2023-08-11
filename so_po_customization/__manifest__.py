376c4c53360e4e4eca330206906ca3a9219df46b
fazeelshahzad <49979617+fazeelshahzad@users.noreply.github.com>
2023-08-11 09:38:13 +0500

    Add files via upload

--- so_po_customization/__manifest__.py
+++ so_po_customization/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'sale', 'purchase', 'stock', 'sale_margin',  'sale_stock', 'approval_so_po', 'product', 'web', 'sale_discount_total'],
+    'depends': ['base', 'account', 'sale', 'purchase', 'stock', 'sale_margin',  'sale_stock', 'approval_so_po', 'product', 'web', 'sale_discount_total', 'l10n_ae'],
 
     # always loaded
     'data': [
