3e24e064e2d4088d04ef616efb2a426f20703e7e
fazeelshahzad <49979617+fazeelshahzad@users.noreply.github.com>
2024-04-29 17:18:05 +0500

    Add files via upload

--- stock_lot_attachment/models/models.py
+++ stock_lot_attachment/models/models.py
@@ -1,15 +1,41 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class StockLotInh(models.Model):
     _inherit = 'stock.lot'
 
-    attachment_ids = fields.Binary(string="Attachment")
+    attachment_ids = fields.Many2many('ir.attachment', string="Add File")
+
+    @api.constrains('attachment_ids')
+    def name_no_duplication(self):
+        if len(self.attachment_ids.ids) > 1:
+            raise UserError('You can attach only 1 file.')
+
+    def action_download(self):
+        for r in self:
+            for rec in r.attachment_ids:
+                return {
+                    "type": "ir.actions.act_url",
+                    "target": "new",
+                    "url": "/web/content?model=ir.attachment&download=true&field=datas&id={}".format(
+                        rec.id),
+                }
 
 
 class StockMoveLineInh(models.Model):
     _inherit = 'stock.move.line'
 
-    attachment_ids = fields.Binary(string="Attachment", related='lot_id.attachment_ids')
+    attachment_ids = fields.Many2many('ir.attachment', string="Attachment", related='lot_id.attachment_ids')
+
+    def action_download(self):
+        for rec in self.attachment_ids:
+            return {
+                "type": "ir.actions.act_url",
+                "target": "new",
+                "url": "/web/content?model=ir.attachment&download=true&field=datas&id={}".format(
+                    rec.id),
+            }
+
