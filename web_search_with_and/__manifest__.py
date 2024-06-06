2f994bd98d7dd13ae33e2855acacf0f70df8e9b2
fazeelshahzad <49979617+fazeelshahzad@users.noreply.github.com>
2024-06-06 18:07:15 +0500

    Add files via upload

--- web_search_with_and/__manifest__.py
+++ web_search_with_and/__manifest__.py
@@ -4,17 +4,16 @@
 
 {
     "name": "Use AND conditions on omnibar search",
-    "version": "17.0.1.0.0",
-    "author": """Sandip SCS, Versada UAB, ACSONE SA/NV, Serincloud,
-    Odoo Community Association (OCA)""",
+    "version": "17.0",
+    "author": """TAMJEED A MUHAMMAD""",
     "license": "AGPL-3",
     "category": "web",
     "website": "https://github.com/OCA/web",
     "depends": ["web"],
     "assets": {
         "web.assets_backend": [
-            # "/web_search_with_and/static/src/js/search_model.esm.js",
-            # "/web_search_with_and/static/src/js/search_bar.esm.js",
+            "/web_search_with_and/static/src/js/search_model.esm.js",
+            "/web_search_with_and/static/src/js/search_bar.esm.js",
         ],
     },
 }
