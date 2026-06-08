08c00b465ce6453d9851f4f44043c486fef6ae2a
fazeelshahzad <49979617+fazeelshahzad@users.noreply.github.com>
2026-06-08 18:52:44 +0500

    Add files via upload

--- stock_lot_attachment/controllers/export_attachments_from_list_view.py
+++ stock_lot_attachment/controllers/export_attachments_from_list_view.py
@@ -17,31 +17,40 @@ _logger = logging.getLogger(__name__)
 class Binary(http.Controller):
 
     @http.route('/web/binary/download_document', type='http', auth="public")
-    def download_document(self, tab_id):
-        """Download attachment method"""
-        new_tab = ast.literal_eval(tab_id)
-        attachment_ids = request.env['ir.attachment'].browse(new_tab)
-        file_dict = {}
-        i = 0
-        for attachment_id in attachment_ids:
-            i += 1
-            file_store = attachment_id.store_fname
-            if file_store:
-                file_name = attachment_id.temp_file_name
-                file_path = attachment_id._full_path(file_store)
-                file_dict["%s:%s" % (file_store, file_name)] = dict(
-                    path=file_path, name=file_name)
-        zip_filename = datetime.now()
-        zip_filename = "%s.zip" % zip_filename
+    def download_document(self, move_line_ids=None, **kw):
+
+        move_line_ids = ast.literal_eval(move_line_ids)
+        move_lines = request.env['stock.move.line'].browse(move_line_ids)
+
         bitIO = BytesIO()
         zip_file = zipfile.ZipFile(bitIO, "w", zipfile.ZIP_DEFLATED)
-        for file_info in file_dict.values():
-            zip_file.write(file_info["path"], file_info["name"])
+
+        for line in move_lines:
+            for attachment in line.attachment_ids:
+
+                file_store = attachment.store_fname
+                if not file_store:
+                    continue
+
+                file_path = attachment._full_path(file_store)
+
+                file_name = (
+                    f"{line.so_no} - "
+                    f"{line.quantity} "
+                    f"{line.product_uom_id.name} - "
+                    f"{attachment.name}"
+                )
+
+                zip_file.write(file_path, file_name)
+
         zip_file.close()
-        return request.make_response(bitIO.getvalue(),
-                                     headers=[('Content-Type',
-                                               'application/x-zip-compressed'),
-                                              ('Content-Disposition',
-                                               content_disposition(
-                                                   zip_filename))]
-                                     )
+
+        zip_filename = "%s.zip" % datetime.now()
+
+        return request.make_response(
+            bitIO.getvalue(),
+            headers=[
+                ('Content-Type', 'application/x-zip-compressed'),
+                ('Content-Disposition', content_disposition(zip_filename))
+            ]
+        )
